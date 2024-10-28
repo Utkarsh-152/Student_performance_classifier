31f9c27cd7505517c1eb72e43210dbae6e0e0c55
Utkarsh Tripathi <shreytripathi2004@gmail.com>
2024-10-28 12:57:51 +0530

    preprocessor.pkl file

---
+++ src/components/data_transformation.py
@@ -0,0 +1,101 @@
+import os 
+import sys
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from src.exception import custom_exception
+from src.logger import logging
+from src.utils import save_object
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformation_obj(self):
+        '''
+            this fuction is responsible for the data transformation
+        '''
+        try:
+            numerical_columns = ['reading_score', 'writing_score']
+            categorical_columns = ['gender', 
+                                   'race_ethnicity', 
+                                   'parental_level_of_education', 
+                                   'lunch', 
+                                   'test_preparation_course'
+                                   ]
+            numerical_pipeline = Pipeline(
+                steps=[
+                    ('imputer',SimpleImputer(strategy='median')),
+                    ('scalar', StandardScaler())
+                ]
+            )
+            categorical_pipeline = Pipeline(
+                steps=[
+                    ('imputer', SimpleImputer(strategy="most_frequent")),
+                    ('one_hot_encoder', OneHotEncoder(sparse_output=False)),
+                    ('scaler', StandardScaler(with_mean=False))
+                ]
+            )
+            logging.info(f"Numerical columns: {numerical_columns}")
+            logging.info(f"categorical columns: {categorical_columns}")
+
+            preprocessor = ColumnTransformer(
+                [
+                    ('numerical_pipeline', numerical_pipeline,numerical_columns),
+                    ('categorical_pipeline', categorical_pipeline,categorical_columns)
+                ]
+            )
+
+            return preprocessor
+        except Exception as e:
+            raise custom_exception(e,sys)
+    
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed")
+            logging.info("obtaining preprocessing obj")
+
+            preprocessing_obj= self.get_data_transformation_obj()
+
+            target_column_name = "math_score"
+
+            input_features_train_df = train_df.drop(columns=target_column_name,axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_features_test_df = test_df.drop(columns=target_column_name,axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info(f"Applying preprocessing object on training dataframe and test dataframe")
+
+            input_features_train_arr = preprocessing_obj.fit_transform(input_features_train_df)
+            input_features_test_arr = preprocessing_obj.transform(input_features_test_df)
+
+            train_arr = np.c_[input_features_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_features_test_arr, np.array(target_feature_test_df)]
+
+            logging.info("saved preprocessing object")
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+        except Exception as e:
+            raise custom_exception(e,sys)
